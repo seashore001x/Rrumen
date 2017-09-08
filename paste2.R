6553a06fcc75dea066aeb550eed2b31490c19a12
seashore001x <sean001x@live.com>
2017-09-08 10:10:17 +0800

    suppress when paste

---
+++ paste2.R
@@ -0,0 +1,8 @@
+# suppress NA when paste
+paste2 <- function(...,sep=", ") {
+  L <- list(...)
+  L <- lapply(L,function(x) {x[is.na(x)] <- ""; x})
+  gsub(paste0("(^",sep,"|",sep,"$)"),"",
+       gsub(paste0(sep,sep),sep,
+            do.call(paste,c(L,list(sep=sep)))))
+}
